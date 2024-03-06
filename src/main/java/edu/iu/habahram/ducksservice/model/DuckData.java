9c79c32401a219259e0e778b190cd95cd93c28ca
antho_0war6 <aceresol@iu.edu>
2024-03-06 09:16:32 -0500

    new

--- src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
+++ src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
@@ -1,6 +1,16 @@
 package edu.iu.habahram.ducksservice.model;
 
-public record DuckData(int id, String type) {
+public record DuckData(int id, String type) implements Comparable<DuckData{
+
+    @Override
+    public int compareTo(DuckData other) {
+        DuckType thisType = DuckType.toEnum(this.type().toLowerCase());
+        DuckType otherType = DuckType.toEnum(other.type().toLowerCase());
+        if (thisType != null && otherType != null) {
+            return thisType.compareTo(otherType);
+        }
+        return 0;
+    }
 
     public String toLine() {
         return String.format("%1$s,%2$s", id(), type());
